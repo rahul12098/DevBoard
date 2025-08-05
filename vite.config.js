a12c8e68b6bfabf034cac8e7c4cb9c9003590cd1
unknown <rhimahapatra@gmail.com>
2025-08-05 18:31:28 +0530

    Deploying to azure , updated yml5

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
+  base: './', // Ensures relative paths
+  build: {
+    outDir: 'build', // Azure prefers 'build' over 'dist'
   },
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000', // Only applies in dev
+    },
+  },
 })
